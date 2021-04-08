88354b1e40663dbf2f8481c7459e93602d258884
Rohan Grover <grohan95@gmail.com>
2021-04-08 22:23:16 +0530

    added needed files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# NextJS With Module Federation Example for :
-# "[Let’s Build Micro Frontends with NextJS and Module Federation!](https://medium.com/the-hamato-yogi-chronichels/lets-build-micro-frontends-with-nextjs-and-module-federation-b48c2c916680)"
+Build Micro Frontends with NextJS and Module Federation
 
 ## How to run:
+
 1. run `yarn prepare`
 2. run `yarn run-app1`
 3. add another command line instance and run `yarn run-app2`
--- app1/components/nav.js
+++ app1/components/nav.js
@@ -1,19 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
 
-const Nav = () => {
+const Nav = ({ color }) => {
   return (
     <nav
       style={{
-        background: 'hotpink',
-        width: '100%',
-        height: '100px',
-        color: 'white',
-        textAlign: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '24px',
-      }}>
+        background: `${color ? color : "darkgreen"}`,
+        width: "100%",
+        height: "100px",
+        color: "white",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "24px",
+        position: "fixed",
+        top: "0px",
+        margin: "0px",
+      }}
+    >
       Nav
     </nav>
   );
